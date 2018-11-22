a74e834f819f1043f65a389f7a0c2285bd3b300f
官万晓 <gwx@spacetech.com.cn>
2018-11-22 18:40:36 +0800

    radio

--- src/page/modules/practice/array/twoPoint/index.vue
+++ src/page/modules/practice/array/twoPoint/index.vue
@@ -3,6 +3,8 @@
     <div class="button"
       @click="onFindIndexClick">找出目标值最小序号</div>
     <div @click="onExpandArrayClick">展开数组</div>
+    <div @click="onMinClick">最小两个数</div>
+    <div @click="onAllInStringClick">判断字符串是否包含(全大写)</div>
   </div>
 </template>
 
@@ -15,7 +17,8 @@ export default {
     return {
       numberList: [1, 1, 2, 2, 4, 4, 4, 6, 6],
       index: 0,
-      expandList: [[112, [1544, [1]]], 1]
+      expandList: [[112, [1544, [1]]], 1],
+      minList: [3, 1, 5, 1, 7, 2]
     }
   },
 
@@ -27,6 +30,33 @@ export default {
     onExpandArrayClick() {
       const list = expandArray(this.expandList)
       console.log(list)
+    },
+    onFindMinClick(arr) {
+      const length = arr.length
+      for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < length - 1; j++) {
+          if (arr[j] > arr[j + 1]) {
+            ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+          }
+        }
+      }
+      // console.log(arr)
+      return arr[0] + arr[1]
+    },
+    onMinClick() {
+      this.onFindMinClick(this.minList)
+      console.log(this.onFindMinClick(this.minList))
+    },
+    onAllInStringClick() {
+      const str1 = 'ADF'
+      const str2 = 'DF'
+      const length = str2.length
+      const statusList = []
+      for (let i = 0; i < length; i++) {
+        statusList.push(new RegExp(str2.charAt(i)).test(str1))
+      }
+
+      return statusList.every(item => item && /[A-Z]/.test(str1 + str2))
     }
   }
 }
--- src/page/modules/practice/other/radio/index.vue
+++ src/page/modules/practice/other/radio/index.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <label for="username"
-      accesskey="1">1:</label>
-    <input type="radio"
-      id="username"
-      tabindex="11" />
-    <label for="user"
-      accesskey="1">2:</label>
-    <input type="radio"
-      id="user"
-      tabindex="12" />
-    <label for="name"
-      accesskey="1">3:</label>
-    <input type="radio"
-      id="name"
-      tabindex="3" />
+    <div class="example">
+      <label v-for="(item,index) in radioList"
+        :key="item.value"
+        :for="item.linkLabel"
+        :accesskey="index">
+        <span class="content__input">
+          <span class="radio__replace"
+            :class="{'radio__replace--checked':selectedRadio===item.value,'radio__replace--disable':item.isDisabled}">
+          </span>
+          <input v-model="selectedRadio"
+            type="radio"
+            class="radio__button"
+            name="radio"
+            :id="item.linkLabel"
+            :tabindex="index"
+            :value="item.value"
+            :disabled="item.isDisabled"
+            @focus="item.isFocus = true"
+            @blur="item.isFocus = false" />
+        </span>
+        <span class="content__text">{{item.label}}</span>
+      </label>
+    </div>
+    <div :class="{test:isShow}"
+      @click="onRedClick">
+      <div class="text__item"></div>
+    </div>
   </div>
 </template>
 
@@ -23,12 +35,119 @@ export default {
   name: 'radio',
 
   data() {
-    return {}
+    return {
+      radioList: [
+        {
+          linkLabel: 'label1',
+          value: '1',
+          isDisabled: false,
+          isFocus: false,
+          label: '标签1'
+        },
+        {
+          linkLabel: 'label2',
+          value: '2',
+          isDisabled: false,
+          isFocus: false,
+          label: '标签2'
+        },
+        {
+          linkLabel: 'label3',
+          value: '3',
+          isDisabled: true,
+          isFocus: false,
+          label: '标签3'
+        }
+      ],
+      selectedRadio: '1',
+      isShow: false
+    }
   },
 
-  methods: {}
+  methods: {
+    onRedClick() {
+      this.isShow = !this.isShow
+    }
+  }
 }
 </script>
 
 <style lang='scss' scoped>
+.example {
+  display: flex;
+  margin-top: 40px;
+  width: 300px;
+  justify-content: space-around;
+  .content {
+    &__text {
+      color: #999;
+    }
+    &__input {
+      position: relative;
+      .radio {
+        &__replace {
+          border: 1px solid #dcdfe6;
+          border-radius: 100%;
+          width: 14px;
+          height: 14px;
+          background-color: #fff;
+          position: relative;
+          cursor: pointer;
+          display: inline-block;
+          box-sizing: border-box;
+          z-index: 999;
+          transition: 0.15s ease-in;
+          &--checked {
+            border-color: #409eff;
+            background-color: #409eff;
+          }
+          &--disable {
+            cursor: not-allowed;
+          }
+          &:after {
+            width: 4px;
+            height: 4px;
+            border-radius: 100%;
+            background-color: #fff;
+            content: '';
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+          }
+        }
+        &__button {
+          opacity: 0;
+          outline: none;
+          position: absolute;
+          z-index: 1;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          margin: 0;
+        }
+      }
+    }
+  }
+}
+
+// 通过层级来控制伪类的样式，给定类名，通过变量来控制类名的有无，有的时候提高层级改变伪类的权重
+.text__item {
+  &:after {
+    content: '';
+    width: 30px;
+    height: 30px;
+    background-color: #f00;
+    position: absolute;
+    bottom: 20px;
+  }
+}
+.test {
+  .text__item {
+    &:after {
+      background-color: #ff0;
+    }
+  }
+}
 </style>
