fe4b2401e4d68c32a51f290ba2234c8c5c96cf90
官万晓 <gwx@spacetech.com.cn>
2018-12-14 18:17:15 +0800

    事件

--- src/page/modules/practice/other/echart/index.vue
+++ src/page/modules/practice/other/echart/index.vue
@@ -15,13 +15,6 @@ export default {
     const myChart = echarts.init(document.getElementById('main'))
     const option = {
       color: ['#3398DB'],
-      // tooltip: {
-      //   trigger: 'axis',
-      //   axisPointer: {
-      //     // 坐标轴指示器，坐标轴触发有效
-      //     type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-      //   }
-      // },
       grid: {
         left: '3%',
         right: '3%',
@@ -75,6 +68,9 @@ export default {
       ]
     }
     myChart.setOption(option)
+    myChart.on('mouseover', function(params) {
+      console.log(params)
+    })
   },
   data() {
     return {}
